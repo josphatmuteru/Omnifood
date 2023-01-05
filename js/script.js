af5bb757c0f7a3cae30643f4eb4411476051b93c
Josphat Muteru <58342288+josphatmuteru@users.noreply.github.com>
2023-01-05 11:11:04 +0300

    Implementing a Sticky Navigation

--- js/script.js
+++ js/script.js
@@ -40,6 +40,32 @@ links.forEach((link) => {
 });
 
 ///////////////////////////////////////////////////////////
+// IMPLEMENTING STICKY NAVIGATION
+
+const sectionHeroEl = document.querySelector(".section-hero");
+
+const obs = new IntersectionObserver(
+  function (entries) {
+    const ent = entries[0];
+
+    if (!ent.isIntersecting) {
+      document.body.classList.add("sticky");
+    }
+
+    if (ent.isIntersecting) {
+      document.body.classList.remove("sticky");
+    }
+  },
+  {
+    root: null,
+    threshold: 0,
+    rootMargin: "-80px",
+  }
+);
+
+obs.observe(sectionHeroEl);
+
+///////////////////////////////////////////////////////////
 // Fixing flexbox gap property missing in some Safari versions
 function checkFlexGap() {
   var flex = document.createElement("div");
